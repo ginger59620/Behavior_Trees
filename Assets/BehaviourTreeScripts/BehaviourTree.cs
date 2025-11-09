8234c94fe92cbe7be2a19fa66cb1d7963b64f240
ginger59620 <a59620@alunos.ipb.pt>
2025-11-09 11:34:59 +0000

    Nodes

---
+++ Assets/BehaviourTreeScripts/BehaviourTree.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourTree : Node
+{
+    public BehaviourTree()
+    {
+        name = "Tree";
+    }
+
+    public BehaviourTree(string n)
+    {
+        name = n;
+    }
+
+    public void PrintTree()
+    {
+
+    }
+}
---
+++ Assets/BehaviourTreeScripts/RobberBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobberBehaviour : MonoBehaviour
+{
+    BehaviourTree tree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tree = new BehaviourTree();
+        Node steal = new Node("Steal Something");
+        Node goToDiamond = new Node("Go To Diamond");
+        Node goToVan = new Node("Go To Van");
+
+        steal.AddChild(goToDiamond);
+        steal.AddChild(goToVan);
+        tree.AddChild(steal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
